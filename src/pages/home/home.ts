4eb13b7e7987702bab8bcfd8d22b66a2c401c668
Javier Gómez Pereda <gopejavi@gmail.com>
2017-12-27 10:53:45 +0100

    Added ability to change location, like 'Miami, FL' or 'Boston, MA'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -13,17 +14,36 @@ export class HomePage {
     state: string
   }
 
-  constructor(public navCtrl: NavController, private weatherProvider: WeatherProvider) {
-
+  constructor(
+    public navCtrl: NavController,
+    private weatherProvider: WeatherProvider,
+    private storage: Storage
+  ) {
+    this.storage.get('location').then((val) => {
+      if (val != null) {
+        let location = JSON.parse(val);
+        this.city = location.city;
+        this.state = location.state;
+      } else {
+        this.city = 'Miami';
+        this.state = 'FL';
+      }
+    });
   }
 
   ionViewWillEnter() {
-    this.location = {
-      city: 'Miami',
-      state: 'FL'
-    }
-    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
-      this.weather = weather.current_observation;
-    });
+    this.storage.get('location').then((val => {
+      if(val != null) {
+        this.location = JSON.parse(val);
+      } else {
+        this.location = {
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+      this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+        this.weather = weather.current_observation;
+      });
+    }));
   }
 }
